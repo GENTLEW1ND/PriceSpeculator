467869e75a2fb3d2e1550f05d3b722eb970cdac2
GENTLEW1ND <rajchakraborty029@gmail.com>
2025-03-11 12:09:23 +0530

    Done with template file and folder structure created

--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 import os
-import pathlib as Path
+from pathlib import Path
 import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
@@ -32,3 +32,11 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     
     filedir, filename = os.path.split(filepath)
+    
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Created file: {filepath}")
